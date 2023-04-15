e2ea91fe61aaae22315adccfcbb1a48dac12c107
Sehii Perevertniuk <s.perevertnyuk@gmail.com>
2023-04-15 23:47:37 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from "@vimeo/player";
+
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+
+function getTimeCode(data) {
+    localStorage.setItem(LOCAL_STORAGE_KEY, data.seconds);
+}
+
+player.on('timeupdate', throttle(getTimeCode, 1000));
+
+
+player.setCurrentTime(localStorage.getItem(LOCAL_STORAGE_KEY)) || 0;
 
